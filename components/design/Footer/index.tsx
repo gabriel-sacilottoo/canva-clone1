c6dc9adf14d6f7f1b41312c18309ad7936f4dd89
Hugo dos Anjos Pena <hugodosanjos.pena@gmail.com>
2025-11-05 22:00:20 -0300

    Delete components directory

--- components/design/Footer/index.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <div className="h-[52px] dark:bg-dark w-full flex items-center overflow-x-auto p-2 gap-x-1 shrink-0 px-4 flex-row-reverse bottom-0 fixed">
-      Footer
-    </div>
-  );
-};
-
-export default Footer;
